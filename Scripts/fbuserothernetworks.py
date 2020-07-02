8264f2f26f3a1f8a87eefce17b1ccd82d91d973b
Frank <44414671+TechNightmare@users.noreply.github.com>
2020-07-02 14:14:30 +0200

    Update fbuserothernetworks.py

--- Scripts/fbuserothernetworks.py
+++ Scripts/fbuserothernetworks.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 print ("Database connection established")
 
 # get Connection
-connection = cx_Oracle.connect("S865745", "student", "localhost/oracle")
+connection = cx_Oracle.connect(username, password, "localhost/oracle")
 cursor = connection.cursor()
 
 y = []
